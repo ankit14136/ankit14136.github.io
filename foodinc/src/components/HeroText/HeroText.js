35140e8fca4518fa6df8e948fcf6db077cd17fd3
akg136 <ankit14136@iiitd.ac.in>
2018-12-04 15:20:23 +0530

    another try

---
+++ foodinc/src/components/HeroText/HeroText.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HeroText.css';
+
+const HeroText = () => {
+		return(
+		<div className='w-50 sec'>
+			<div>
+				<h1 className="pl4 food">Find Me Food!</h1>
+			</div>
+		</div>
+	);
+}
+
+export default HeroText;
