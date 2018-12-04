7cd32ec23f630d49d22ea4e9bfa5341fccf8edbe
akg136 <ankit14136@iiitd.ac.in>
2018-12-04 16:08:51 +0530

    oops

---
+++ foodinc/src/components/Video/Video.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Video.css';
+
+const Video = () => {
+		return (
+			<div className='bg-yellow  w-50 sec'>
+				<div className='black f3'>
+					{`your current rank is...`}
+				</div>
+				<div className='black f1'>
+					#11
+				</div>
+			</div>
+		);
+}
+
+export default Video;
